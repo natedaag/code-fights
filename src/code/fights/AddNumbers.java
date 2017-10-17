676544a7f192571449b0411a8a61af14b85098eb
natedaag <natedaage@yahoo.com>
2017-10-17 15:56:44 -0600

    method add() successfully adds 2 ints

---
+++ src/code/fights/AddNumbers.java
@@ -0,0 +1,17 @@
+package code.fights;
+
+public class AddNumbers {
+
+	public static void main(String[] args) {
+		add(1, 2);
+	}
+	
+	static int add(int param1, int param2) {
+		
+		int total;
+		total = param1 + param2;
+		System.out.print(total);
+		return total;
+	}
+
+}
