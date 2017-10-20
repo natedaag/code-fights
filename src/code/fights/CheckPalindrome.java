ede95daeb5037f0987dc4ef1212434d8d1148ca0
Nathan <natedaage@yahoo.com>
2017-10-20 13:35:28 -0600

    adjacentelementsproduct solved!

--- src/code/fights/CheckPalindrome.java
+++ src/code/fights/CheckPalindrome.java
@@ -3,16 +3,17 @@ package code.fights;
 public class CheckPalindrome {
 
 	public static void main(String[] args) {
-		checkPalindrome("");
+		checkPalindrome("a");
 	}
 	
 	static boolean checkPalindrome(String inputString) {
-		String str = null;
+		String str;
 		str = new StringBuilder(inputString).reverse().toString();
 		if(inputString.equals(str)){
-			System.out.print(str);
+			System.out.print("true");
 			return true;
 		} else {
+		  System.out.print("false");
 		return false;
 		}
 	}
