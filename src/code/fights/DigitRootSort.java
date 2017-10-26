85c31a807ad1e1bdbbf722698037db59ecbb4fb5
natedaag <natedaage@yahoo.com>
2017-10-26 16:49:54 -0600

    doesnt even compile - giving up for the moment

---
+++ src/code/fights/DigitRootSort.java
@@ -0,0 +1,28 @@
+package code.fights;
+
+public class DigitRootSort {
+
+	public static void main(String[] args) {
+		int[] input = {13, 20, 7, 4};
+		digitRootSort(input);
+	}
+	
+	static int[] digitRootSort(int[] a) {
+		
+		for (int i = 0; i < a.length; i++){
+			
+			if (a[i] > 9){
+				String str = toString(a[i]);
+				
+			}
+		}
+		return null;
+	}
+
+	static String toString(int i) {
+		return i;
+	}
+
+	
+
+}
