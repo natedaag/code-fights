9f2d1b2981cd5dfb4325c26259d56b0f07686ad1
natedaag <natedaage@yahoo.com>
2017-10-30 14:24:32 -0600

    a bit easier to read and make sense of - 2 for loops.

--- src/code/fights/AlmostIncreasingSequence.java
+++ src/code/fights/AlmostIncreasingSequence.java
@@ -8,26 +8,44 @@ public class AlmostIncreasingSequence {
   }
   
   static boolean almostIncreasingSequence(int[] sequence) {
-	    boolean sequential = true;
-	    for(int i = 0; i < sequence.length - 1; i++) {
-	        if(sequence[i] >= sequence[i + 1]) {
-	            if(sequential) {
-	                if(i != 0 && i != sequence.length-2) {
-	                    if(sequence[i + 1] <= sequence[i - 1]) {
-	                        if(sequence[i + 2] <= sequence[i]) {
-	                            return false;
-	                        }
-	                        i++;
-	                    }
-	                }
-	                sequential = false;
-	            } else {
-	                return false;
-	            }
+	    boolean flag = true;
+	    int seq1 = 0 ;
+	    int seq2 = 0;
+	    for(int i = 0; i < sequence.length - 1; i++){
+	        if(sequence[i]>=sequence[i+1]){
+	        	seq1++;
+	    }
+	    for(int k = 0; k < sequence.length - 2; k++){
+	        if(sequence[k] >= sequence[k + 2]){
+	        	seq2++;
 	        }
 	    }
-	    return true;
+	    if(seq1+seq2>2){
+	    	flag=false;
 	    }
+	}
+		return flag;
+  }
+//	    boolean sequential = true;
+//	    for(int i = 0; i < sequence.length - 1; i++) {
+//	        if(sequence[i] >= sequence[i + 1]) {
+//	            if(sequential) {
+//	                if(i != 0 && i != sequence.length-2) {
+//	                    if(sequence[i + 1] <= sequence[i - 1]) {
+//	                        if(sequence[i + 2] <= sequence[i]) {
+//	                            return false;
+//	                        }
+//	                        i++;
+//	                    }
+//	                }
+//	                sequential = false;
+//	            } else {
+//	                return false;
+//	            }
+//	        }
+//	    }
+//	    return true;
+//	    }
 
 
 }
