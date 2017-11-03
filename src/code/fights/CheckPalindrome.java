0f7b52801d48916679b4312ec7afacfba1a1a0b4
natedaag <natedaage@yahoo.com>
2017-11-03 15:33:17 -0600

    digitroot sort still doesn't work, but it's closer

--- src/code/fights/CheckPalindrome.java
+++ src/code/fights/CheckPalindrome.java
@@ -3,18 +3,24 @@ package code.fights;
 public class CheckPalindrome {
 
 	public static void main(String[] args) {
-		checkPalindrome("a");
+		checkPalindrome("5aa5aa5");
 	}
 	
 	static boolean checkPalindrome(String inputString) {
+		boolean isPalindrome = false;
+		
+		
+		
 		String str;
 		str = new StringBuilder(inputString).reverse().toString();
 		if(inputString.equals(str)){
 			System.out.print("true");
-			return true;
+			isPalindrome = true;
 		} else {
 		  System.out.print("false");
-		return false;
+		isPalindrome = false;
 		}
+		
+		return isPalindrome;
 	}
 }
--- src/code/fights/DigitRootSort.java
+++ src/code/fights/DigitRootSort.java
@@ -3,15 +3,33 @@ package code.fights;
 public class DigitRootSort {
 
 	public static void main(String[] args) {
-		int[] input = {13, 20, 7, 4};
+		int[] input = {13, 4};
 		digitRootSort(input);
 	}
 	
 	static int[] digitRootSort(int[] a) {
-		
-				
+	    int[] b = new int[a.length];
+		int start; // takes the int from the array
+		int workingRoot; // if it's 10-100, % 10
+		int inPlace = 0;
+		int digitSum = 0; // actual digit sum value
+		int workingTen = 0; // starts to manipulate it. <10=ok. >10 mod  to get other digit
+		for(int i = 0; i < a.length; i++){
+			start = a[i];
+			if (start < 10){
+				inPlace = start;
+				System.out.println(inPlace);
+			}
+			if(start >= 10 && start < 100){
+				workingTen = start / 10;
+				workingRoot = start % 10;
+				digitSum = workingTen + workingRoot;
+				System.out.println(digitSum);
+			}
 			
-		return null;
+		}
+				
+		return b;
 	}
 
 
@@ -19,3 +37,14 @@ public class DigitRootSort {
 	
 
 }
+
+
+
+
+
+
+
+
+
+
+
